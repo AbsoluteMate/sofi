8ac5ca83374148a98fc5bd546bd5e80ba35ee51c
Mate <50421605+AbsoluteMate@users.noreply.github.com>
2022-12-02 20:02:15 +0100

    Nuovo aggiornamento

--- index.php
+++ index.php
@@ -1,111 +1,110 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "sofi";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+
+require("database.php");
+
+$old_nome = "";
+$old_cognome = "";
+$where = "";
+if (isset($_POST["nome"]) && isset($_POST["cognome"])) {
+    $old_nome = $_POST["nome"];
+    $old_cognome = $_POST["cognome"];
+    $where = "WHERE calciatori.nome = '" . $_POST["nome"] . "' AND calciatori.cognome = '" . $_POST["cognome"] . "'";
 }
 
-$sql = "SELECT studenti.nome as n_studente, studenti.cognome as c_studente, scuole.nome as n_scuole FROM studenti INNER JOIN scuole ON scuole.id = studenti.scuole_fk";
-$listaStudenti = $conn->query($sql);
-
-$sql2 = "SELECT * FROM scuole";
-$listaScuole = $conn->query($sql2);
+$sql = "SELECT COUNT(fk_partite.id_partita) as part_p, calciatori.id as id_c, calciatori.nome as nome_c, calciatori.cognome as cog_c,calciatori.nazionalita as naz_c,squadre.nome as n_s
+        FROM calciatori INNER JOIN fk_squadre ON calciatori.id = fk_squadre.id_calciatore
+        INNER JOIN squadre ON squadre.id = fk_squadre.id_squadra
+        INNER JOIN fk_partite ON squadre.id = fk_partite.id_squadra " . $where . "
+        GROUP BY calciatori.id";
+$lista = $conn->query($sql) or die($conn->error);
 $conn->close();
 ?>
 
 <!DOCTYPE html>
-<html lang="ita">
+<html lang="en">
 
 <head>
-    <style>
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 75%;
-            margin: 50px auto;
-        }
-
-        td,
-        th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dddddd;
-        }
-    </style>
-    <title>Test</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h2 style="text-align:center;">Cerca un studente</h2>
-    <form action="/search.php">
-        <label for="fname">Nome:</label><br>
-        <input type="text" id="fname" name="fname" value="John" required><br>
-        <label for="lname">Cognome:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe" required><br><br>
-        <input type="submit" value="Submit">
-    </form>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sofia Pegoraro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <h1 class="text-center mb-5 mt-5">Lista giocatori/squadra</h1>
+        <div class="row">
+            <div class="col-6">
+                <form class="form-inline" action="/index.php" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nome calciatore</label>
+                        <input type="text" class="form-control" name="nome" placeholder="Nome" value="<?php echo $old_nome ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Cognome Calciatore</label>
+                        <input type="text" class="form-control" name="cognome" placeholder="Cognome" value="<?php echo $old_cognome ?>">
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2">Cerca</button>
 
-    <h2 style="text-align:center;">Tabella studenti-scuola</h2>
+                </form>
+            </div>
+            <div class="col-6">
 
-    <table>
-        <tr>
-            <th>Nome</th>
-            <th>Cognome</th>
-            <th>Scuola</th>
-        </tr>
-        <?php
-        if ($listaStudenti->num_rows > 0) {
-            // output data of each row
-            while ($row = $listaStudenti->fetch_assoc()) {
-                echo '<tr>';
-                echo '<td>' + $row["n_studente"] + '</td>';
-                echo '<td>' + $row["c_studente"] + '</td>';
-                echo '<td>' + $row["n_scuola"] + '</td>';
-                echo '</tr>';
-            }
-        } else {
-            echo '<tr>';
-            echo '<td colspan="3">Nessun risultato trovato</td>';
-            echo '</tr>';
-        }
-        ?>
-    </table>
-
-    <h2 style="text-align:center;margin-top:50px;">Inserisci un nuovo studente</h2>
-    <form action="/addStudente.php">
-        <label for="fname">Nome:</label><br>
-        <input type="text" id="fname" name="fname" value="John" required><br>
-        <label for="lname">Cognome:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe" required><br><br>
-        <input type="submit" value="Submit">
-        <select name="scuola" id="scuola" required>
-            <?php
-            if ($listaScuole->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo '<option value=' + $row["id"] + '>' + $row["nome"] + '</option>';
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Giocatore</th>
+                    <th scope="col">Nazionalità</th>
+                    <th scope="col">Squadra</th>
+                    <th scope="col">Partite giocate</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($lista->num_rows > 0) {
+                    $counter = 0;
+                    // output data of each row
+                    while ($row = $lista->fetch_assoc()) {
+                        echo '<tr>';
+                        echo '<td scope="row">' .  $counter . '</td>';
+                        echo '<td>' . $row["nome_c"] . ' ' . $row["cog_c"] . '</td>';
+                        echo '<td>' . $row["naz_c"] . '</td>';
+                        echo '<td>' . $row["n_s"] . '</td>';
+                        echo '<td>' . $row["part_p"] . '</td>';
+                        echo '</tr>';
+                        $counter++;
+                    }
+                } else {
+                    echo '<tr>';
+                    echo '<td colspan="5">Nessun risultato trovato</td>';
+                    echo '</tr>';
                 }
-            } else {
-                echo '<option disabled>Nessuna scuola registrata</option>';
-            }
-            ?>
-        </select>
-    </form>
-
-    <h2 style="text-align:center;margin-top:50px;">Inserisci una nuova scuola</h2>
-    <form action="/addScuola.php">
-        <label for="scname">Nome:</label><br>
-        <input type="text" id="scname" name="scname"><br>
-        <input type="submit" value="Submit">
-    </form>
-
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 
 </body>
--- table.sql
+++ table.sql
@@ -1,11 +1,47 @@
-CREATE TABLE scuole (
-    PRIMARY KEY (id),
-    nome varchar NOT NULL,
-);
+CREATE TABLE `calciatori` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cognome` varchar(255) NOT NULL,
+  `nazionalita` varchar(255) NOT NULL
+) 
 
-CREATE TABLE studenti (
-    PRIMARY KEY (id),
-    nome varchar NOT NULL,
-    cognome varchar NOT NULL,
-    FOREIGN KEY (scuola_fk) REFERENCES Scuola(id)
-);
+CREATE TABLE `fk_partite` (
+  `id_squadra` int(11) NOT NULL,
+  `id_partita` int(11) NOT NULL
+) 
+
+CREATE TABLE `fk_squadre` (
+  `id_calciatore` int(11) NOT NULL,
+  `id_squadra` int(11) NOT NULL
+) 
+
+CREATE TABLE `partite` (
+  `id` int(11) NOT NULL,
+  `stadio` varchar(255) NOT NULL
+) 
+
+CREATE TABLE `squadre` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `stato` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `calciatori`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `partite`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `squadre`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `calciatori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `partite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `squadre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
